9d03c85f05ff6cf7147cf2611f2a0ae8afbe6df9
Natalie <daly.na@northeastern.edu>
2023-03-31 15:55:53 -0400

    angle calc. fixes, marker fixes, confirmed rviz

--- scripts/depth_estimation.py
+++ scripts/depth_estimation.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python
 
-import cmath
+import math
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import LaserScan
-from darknet_ros_msgs.msg import BoundingBoxes, BoundingBox
+from darknet_ros_msgs.msg import BoundingBoxes
 from visualization_msgs.msg import Marker, MarkerArray
 from geometry_msgs.msg import Point
+from nav_msgs.msg import Odometry
 import numpy as np
 
-RASPBERRY_PI_ANGLE = np.pi * 62.2 / 180
-RASPBERRY_PI_WIDTH = 2592
-RASPBERRY_PI_HEIGHT = 1944
+HORIZONTAL_ANGLE = np.pi * 70.42 / 180
+IMAGE_WIDTH = 720 #1920
+IMAGE_HEIGHT = 1080
 
 current_laserscan = LaserScan()
 current_boundingboxes = BoundingBoxes()
+current_position = Odometry()
 
 def position_to_angle(position):
-    return (RASPBERRY_PI_ANGLE / 2) - ((RASPBERRY_PI_WIDTH - position) / RASPBERRY_PI_WIDTH) * RASPBERRY_PI_ANGLE
+    return position / IMAGE_WIDTH * HORIZONTAL_ANGLE - HORIZONTAL_ANGLE / 2
 
 def calculate_depth():
-    marker_list = MarkerArray()
     global current_boundingboxes
     global current_laserscan
+    global current_position
     boxes = current_boundingboxes
     laserscan = current_laserscan
     rospy.loginfo("calculating depth...")
@@ -41,34 +43,41 @@ def calculate_depth():
         ranges = []
         for (i, range) in enumerate(laserscan.ranges):
             angle = laserscan.angle_min + laserscan.angle_increment * i
-            rospy.loginfo(angle)
+            if angle >= np.pi:
+                angle -= np.pi*2
+            #if range != float("inf"):
+                #rospy.loginfo(angle)
+                #rospy.loginfo(range)
             if (angle >= angle_min and angle <= angle_max and range >= laserscan.range_min and range <= laserscan.range_max):
-                ranges += range
+                ranges.append(range)
         rospy.loginfo(ranges)
-        mean_range = np.mean(ranges)
-        marker = Marker()
-        marker.header.frame_id = "base_link"
-        marker.header.stamp = rospy.get_time()
-        marker.ns = "my_namespace"
-        marker.id = i
-        marker.type = Marker.POINTS
-        marker.action = Marker.ADD
-        marker.points.append(Point(cmath.sin(angle_center) * mean_range, cmath.cos(angle_center) * mean_range), 0)
-        #marker.pose.position.x = cmath.sin(angle_center) * mean_range
-        #marker.pose.position.y = cmath.cos(angle_center) * mean_range
-        #marker.pose.position.z = 0
-        #marker.pose.orientation.x = 0.0
-        #marker.pose.orientation.y = 0.0
-        #marker.pose.orientation.z = 0.0
-        #marker.pose.orientation.w = 1.0
-        #marker.scale.x = 1
-        #marker.scale.y = 0.1
-        #marker.scale.z = 0.1
-        #marker.color.a = 1.0
-        #marker.color.r = 1.0
-        #marker.color.g = 0.0
-        #marker.color.b = 0.0
-        marker_list.markers.append(marker)   
+        marker_list = MarkerArray()
+        if (ranges != []):
+            mean_range = np.mean(ranges)
+            marker = Marker()
+            marker.header.frame_id = "base_link"
+            marker.header.stamp = rospy.Time.now()
+            marker.ns = "my_namespace"
+            marker.id = i
+            marker.type = marker.CUBE
+            marker.action = marker.ADD
+            marker.pose.position.x = math.cos(angle_center) * mean_range + current_position.pose.pose.position.x
+            marker.pose.position.y = math.sin(angle_center) * mean_range + current_position.pose.pose.position.y
+            marker.pose.position.z = 0
+            marker.pose.orientation.w = 1
+            marker.scale.x = 1
+            marker.scale.y = 1
+            marker.scale.z = 1
+            marker.color.a = 1
+            marker.color.r = 1
+            marker.color.g = 0
+            marker.color.b = 0
+            marker.lifetime = rospy.Duration()
+            
+            marker_list.markers.append(marker)
+        else:
+            rospy.loginfo("No ranges detected")
+    print(current_position)
     rospy.loginfo(marker_list)
     pub.publish(marker_list)
 
@@ -83,7 +92,10 @@ def laserscan_recieved(laserscan):
     global current_laserscan
     #rospy.loginfo(current_laserscan)
     current_laserscan = laserscan
-    #calculate_depth()
+
+def odom_recieved(odom):
+    global current_position
+    current_position = odom
 
 
 if __name__ == '__main__':
@@ -92,6 +104,8 @@ if __name__ == '__main__':
         rospy.init_node('depth_estimation', anonymous=True)
         rospy.Subscriber("scan", LaserScan, laserscan_recieved)
         rospy.Subscriber("darknet_ros/bounding_boxes", BoundingBoxes, boundingbox_recieved)
+        rospy.Subscriber("odom", Odometry, odom_recieved)
+        rospy.loginfo("Waiting for messages...")
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
